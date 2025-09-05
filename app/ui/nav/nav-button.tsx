cf3cfc2560c145cd5812d618be0b9de81171e6a8
David Zuluaga <davidzuluaga@Davids-MacBook-Pro.local>
2025-09-05 00:55:20 -0600

    missing padding

--- app/ui/nav/nav-button.tsx
+++ app/ui/nav/nav-button.tsx
@@ -20,7 +20,7 @@ export default function NavButton({ name, href, icon: Icon }: NavButtonProps) {
     <Link
       href={href}
       className={clsx(
-        "flex items-center gap-2 rounded-full px-4 text-base font-medium transition-all duration-200",
+        "flex items-center gap-2 rounded-full px-4 py-2 text-base font-medium transition-all duration-200",
         "hover:bg-creoSec-100 hover:shadow-sm",
         pathname === href
           ? "bg-creoPri-200 text-white shadow-md"
